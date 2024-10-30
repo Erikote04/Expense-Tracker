b54bf43eb6b6e0a2d4a2fe3c679493a15aeb1f1e
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-30 22:12:24 +0100

    Builiding the Recents view

--- Expense Tracker/Expense Tracker/View/TabBar/RecentsView.swift
+++ Expense Tracker/Expense Tracker/View/TabBar/RecentsView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct RecentsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader {
+            let size = $0.size
+            
+            NavigationStack {
+                ScrollView(.vertical) {
+                    LazyVStack(spacing: 8, pinnedViews: [.sectionHeaders]) {
+                        Section {
+                            
+                        } header: {
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
