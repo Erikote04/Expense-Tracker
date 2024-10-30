593ebfdfece1ab81a35c40213cc77e2597876cb1
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-30 20:40:33 +0100

    Setting up

---
+++ Expense Tracker/Expense Tracker/View/IntroductionView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroductionView.swift
+//  Expense Tracker
+//
+//  Created by Erik Sebastian de Erice Jerez on 30/10/24.
+//
+
+import SwiftUI
+
+struct IntroductionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroductionView()
+}
