288f297d42a201dcbde85a4b76664d7b6871bcaf
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-11-13 20:12:17 +0100

    Implementing widgets

---
+++ Expense Tracker/StatsCardView/StatsCardViewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatsCardViewBundle.swift
+//  StatsCardView
+//
+//  Created by Erik Sebastian de Erice Jerez on 13/11/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatsCardViewBundle: WidgetBundle {
+    var body: some Widget {
+        StatsCardView()
+    }
+}
